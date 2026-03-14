46e1d783b5c9ac7ca058f626ce0713664a5fc70c
sesempurnaproperty <cikguikmalproperty@gmail.com>
2026-03-15 03:48:33 +0800

    Create properties.js

---
+++ data/properties.js
@@ -0,0 +1,23 @@
+const properties = [
+
+{
+id:1,
+title:"Double Storey Terrace",
+location:"Shah Alam",
+price:"RM650,000",
+image:"images/house1.jpg",
+bedroom:4,
+bathroom:3
+},
+
+{
+id:2,
+title:"Apartment Puchong",
+location:"Puchong",
+price:"RM320,000",
+image:"images/house2.jpg",
+bedroom:3,
+bathroom:2
+}
+
+];
